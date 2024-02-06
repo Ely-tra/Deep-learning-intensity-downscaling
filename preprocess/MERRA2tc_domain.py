3dc572027093b6fd9f403e92e0ef322ae3ff75fb
Khanh Luong <kmluong@login1.hsn0.cm.bigred200.uits.iu.edu>
2024-02-05 21:16:31 -0500

    Fixed some bugs, created the first version of CNN_s1, works well

--- preprocess/MERRA2tc_domain.py
+++ preprocess/MERRA2tc_domain.py
@@ -20,6 +20,8 @@ def process_entries(per):
     global starttime
     global endtime
     global faulty
+    if per==0:
+     per=100
     if (count + faulty) % per == 0:
         endtime = timer()
         entries_processed = count + faulty
@@ -410,7 +412,7 @@ def merge_data(csvdataset, tc_name='', years='', minlat = -90.0
   print('Generated ' + str(count) + ' windows.', flush=True) 
 datapath='/N/u/tqluu/BigRed200/@PUBLIC/nasa-merra2-full/'
 csvdataset='/N/project/hurricane-deep-learning/data/tc/ibtracs.ALL.list.v04r00.csv'
-merge_data(csvdataset, regions=['NA','WP', 'EP'], datapath=datapath)
+merge_data(csvdataset, regions=['EP'],years=2001, datapath=datapath)
 #For processing faulty window size, use minlon=171-0.625*3 and maxlon=-171+0.625*3 >>>max-windowsize+3gridsize<<<
 #tc_name (str or None), years (str or None), minlat (float), maxlat (float), minlon (float), maxlon (float), regions (str or None), maxwind (int), minwind (int), maxpres (int), minpres (int), maxrmw (int), minrmw (int), windowsize (tuple), datapath (str)  
 #Define parameters, only csvdataset is required, if no keyword argument is given, the function search for the whole domain            
