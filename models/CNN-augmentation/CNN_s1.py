4bb620b0331ead8b1110810c99407af753ebd475
Ely-tra <157906084+Ely-tra@users.noreply.github.com>
2024-02-14 22:32:38 -0500

    Do some testing

--- models/CNN-augmentation/CNN_s1.py
+++ models/CNN-augmentation/CNN_s1.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 from npy_append_array import NpyAppendArray
+import math
 
 print('Initiation completed.', flush=True)
 
-def dumping_data(root, outdir, outname=['CNNfeatures', 'CNNlabels']):
+def dumping_data(root, outdir, outname=['CNNfeatures', 'CNNlabels'], omit_percent=5):
     """
     Dump data from NetCDF files to NumPy arrays.
 
@@ -21,14 +22,18 @@ def dumping_data(root, outdir, outname=['CNNfeatures', 'CNNlabels']):
     None
     """
     i = 0
-    data_x = []
-    data_y = []
-
     for filename in glob.iglob(root + '*/**/*.nc', recursive=True):
         data = xr.open_dataset(filename)
-        data_array_x = np.array(data[['U', 'V', 'PS', 'T', 'H']].sel(lev=900).to_array())
-        if np.sum(np.isnan(data_array_x))>0:
-            print(filename)
+        data_array_x = np.array(data[['U', 'V', 'T', 'RH', 'SLP']].sel(lev=850).to_array())
+        if np.sum(np.isnan(data_array_x)) / 4 > omit_percent / 100 * math.prod(data_array_x[0].shape):
+            i+=1 
+            if np.sum(np.isnan(data_array_x)) % 4 != 0:
+                print('Oh gawd')
+                break
+            if i % 1000 == 0:
+                print(str(i) + ' dataset processed.', flush=True)
+                break
+            continue
         data_array_x = data_array_x.reshape([1, data_array_x.shape[0],
                                              data_array_x.shape[1], data_array_x.shape[2]])
         data_array_y = np.array([data.VMAX, data.PMIN, data.RMW])  # knots, mb, nmile
