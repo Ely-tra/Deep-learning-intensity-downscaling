dde3cfa5ce2924a76273b2a3722b38bcc52cd8e0
Khanh Luong <kmluong@login2.hsn0.cm.bigred200.uits.iu.edu>
2024-02-28 12:33:35 -0500

    Added model scripts

--- models/CNN-augmentation/buildmodels.py
+++ models/CNN-augmentation/buildmodels.py
@@ -12,31 +12,32 @@ def main(dense_layers=[1],layer_sizes=[32],conv_layers=[3],X=[],y=[], target='VM
     for dense_layer in dense_layers:
         for layer_size in layer_sizes:
             for conv_layer in conv_layers:
-                NAME =root+'model/'+ loss+'-'+str(firstlayer)+"{}-conv-{}-layer-{}-dense.model".format(conv_layer,layer_size,dense_layer)
+                NAME =root+'model/'+'testmodel3'
                 print('--> Running configuration: ',NAME)
                 inputs = keras.Input(shape=X.shape[1:])
                 x = data_augmentation(inputs)
                 #x = layers.Conv2D(filters=256,kernel_size=7, padding='same' ,activation="relu",name="my_conv2d_1")(x)
-                x = layers.Conv2D(filters=firstfilter,kernel_size=7, padding='same',activation=activ,name="my_conv2d_11")(x)
+                x = layers.Conv2D(filters=96,kernel_size=7, padding='same',activation=activ,name="my_conv2d_11")(x)
                 #x = layers.Conv2D(filters=layer_size*2,kernel_size=3, activation="relu",name="my_conv2d_12")(x)
                 x = layers.MaxPooling2D(pool_size=2,name="my_pooling_1")(x)
-                x=tf.keras.layers.BatchNormalization()(x)
-                x = layers.Conv2D(filters=layer_size*2,kernel_size=conv_layer, activation=activ,name="my_conv2d_2")(x)
+                #x=tf.keras.layers.BatchNormalization()(x)
+                x = layers.Conv2D(filters=64,kernel_size=7,padding='same', activation=activ,name="my_conv2d_2")(x)
                 x = layers.MaxPooling2D(pool_size=2,name="my_pooling_2")(x)
-                x=tf.keras.layers.BatchNormalization()(x)
-                x = layers.Conv2D(filters=layer_size*2,kernel_size=conv_layer,activation=activ,name="my_conv2d_3")(x)
-                x = layers.Conv2D(filters=layer_size*2,kernel_size=conv_layer, activation=activ,name="my_conv2d_31")(x)
-                #x = layers.Conv2D(filters=layer_size*4,kernel_size=5,padding='same',activation="relu",name="my_conv2d_32")(x)
+                #x=tf.keras.layers.BatchNormalization()(x)
+                x = layers.Conv2D(filters=128,kernel_size=conv_layer,activation=activ,name="my_conv2d_3")(x)
+                #x = layers.Conv2D(filters=layer_size*2,kernel_size=conv_layer, activation=activ,name="my_conv2d_31")(x)
+                #x = layers.Conv2D(filters=layer_size*4,kernel_size=3,padding='same',activation="relu",name="my_conv2d_32")(x)
                 x = layers.MaxPooling2D(pool_size=2,name="my_pooling_3")(x)
 
-                x=tf.keras.layers.BatchNormalization()(x)
-                x = layers.Conv2D(filters=layer_size*2,kernel_size=3, padding='same', activation=activ,name="my_conv2d_4")(x)
+                #x=tf.keras.layers.BatchNormalization()(x)
+                x = layers.Conv2D(filters=layer_size*4,kernel_size=3, padding='same', activation=activ,name="my_conv2d_4")(x)
                 
-                x = layers.Conv2D(filters=layer_size*2,kernel_size=3, activation=activ,name="my_conv2d_5")(x)
+                #x = layers.Conv2D(filters=layer_size*2,kernel_size=3, activation=activ,name="my_conv2d_5")(x)
                 #x = layers.MaxPooling2D(pool_size=2,name="my_pooling_4")(x)
-                x=tf.keras.layers.BatchNormalization()(x)
+                #x = layers.Conv2D(filters=layer_size*4,kernel_size=3, activation=activ,name="my_conv2d_5")(x)
+                #x=tf.keras.layers.BatchNormalization()(x)
                 x = layers.Flatten(name="my_flatten")(x)
-                x = layers.Dropout(0.3)(x)
+                x = layers.Dropout(0.2)(x)
                 for _ in range(dense_layer):
                     x = layers.Dense(layer_size,activation=activ)(x)                
                 
@@ -46,7 +47,7 @@ def main(dense_layers=[1],layer_sizes=[32],conv_layers=[3],X=[],y=[], target='VM
             
                 callbacks=[keras.callbacks.ModelCheckpoint(NAME,save_best_only=True)]
                 model.compile(loss=loss,optimizer="adam",metrics=['MAE',tf.keras.metrics.RootMeanSquaredError()])
-                history = model.fit(X, y, batch_size=5000, epochs=1000, validation_split=0.1, callbacks=callbacks)
+                history = model.fit(X, y, batch_size=128, epochs=1000, validation_split=0.1, callbacks=callbacks)
     return history
 def resize_preprocess(image, HEIGHT, WIDTH, method):
     image = tf.image.resize(image, (HEIGHT, WIDTH), method=method)
@@ -72,7 +73,7 @@ number_channels=X.shape[3]
 
 
 
-activ='linear'
+activ='relu'
 x_train,y_train = normalize_channels(X,y)
 '''
 for i in range(y.shape[1]):
@@ -83,12 +84,12 @@ x_train=resize_preprocess(x_train, 64,64, 'lanczos5')
 DENSE_LAYER = [2]
 LAYER_SIZES = [32]
 CONV_LAYERS = [3]
-losses=["mean_absolute_percentage_error", "huber_loss", "log_cosh"]
+losses=["mean_absolute_percentage_error", "huber_loss", "log_cosh", 'MSE', 'MAE', tf.keras.losses.MeanAbsolutePercentageError(),tf.keras.losses.MeanSquaredLogarithmicError(),tf.keras.losses.CosineSimilarity()]
 print('Input shape of the X features data: ',X.shape)
 print('Input shape of the y label data: ',y.shape)
 print('Number of input channel extracted from X is: ',number_channels)
 firstfilter=[32]
-for f in firstfilter:
+for loss in ['huber']:
     history = main(dense_layers=DENSE_LAYER,layer_sizes=LAYER_SIZES,conv_layers=CONV_LAYERS,
-               X=x_train,y=y_train, target='VMAX', loss='huber', firstlayer=7,
-               firstfilter=f)
+               X=x_train,y=y_train, target='VMAX', loss=loss, firstlayer=7,
+               firstfilter=32)
