b4d0d5142840538268c344ab465d4a34a1cea83d
Ely-tra <157906084+Ely-tra@users.noreply.github.com>
2024-03-01 23:42:07 -0500

    Update CNN_extract.py, now take also 750mb data

--- models/CNN-augmentation/CNN_extract.py
+++ models/CNN-augmentation/CNN_extract.py
@@ -9,7 +9,7 @@ import math
 
 print('Initiation completed.', flush=True)
 
-def dumping_data(root, outdir, outname=['CNNfeatures', 'CNNlabels'], omit_percent=5):
+def dumping_data(root, outdir, outname=['CNNfeatures9', 'CNNlabels9'], omit_percent=5):
     """
     Dump data from NetCDF files to NumPy arrays.
 
@@ -25,12 +25,13 @@ def dumping_data(root, outdir, outname=['CNNfeatures', 'CNNlabels'], omit_percen
     omit=0
     for filename in glob.iglob(root + '*/**/*.nc', recursive=True):
         data = xr.open_dataset(filename)
-        data_array_x = np.array(data[['U', 'V', 'T', 'RH', 'SLP']].sel(lev=850).to_array())
-        if np.sum(np.isnan(data_array_x)) / 4 > omit_percent / 100 * math.prod(data_array_x[0].shape):
+        data_array_x = np.array(data[['U', 'V', 'T', 'RH']].sel(lev=850).to_array())
+        data_array_x = np.append(data_array_x, np.array(data[['U', 'V', 'T', 'RH', 'SLP']].sel(lev=750).to_array()), axis=0)
+        if np.sum(np.isnan(data_array_x[0:4])) / 4 > omit_percent / 100 * math.prod(data_array_x[0].shape):
             i+=1
             #print(filename + ' omitted', flush=True)
             omit+=1
-            if np.sum(np.isnan(data_array_x)) % 4 != 0:
+            if np.sum(np.isnan(data_array_x[0:4])) % 4 != 0:
                 print('Oh gawd', flush=True)
                 break
             if i % 1000 == 0:
