01a8dfcb03e00b550add2cc72ba534646e0124a1
Ely-tra <157906084+Ely-tra@users.noreply.github.com>
2024-05-15 23:03:26 +0700

    Update TC-Combine_data.py

--- TC-net-cnn/TC-Combine_data.py
+++ TC-net-cnn/TC-Combine_data.py
@@ -4,15 +4,15 @@ root='/N/slate/kmluong/Training_data/'        # Data root
 import numpy as np
 name=['NA','WP','EP']                         # Choosing basins to combine
 for mode in ['13']:                           # Number of layers of data
-  for xy in ['features','labels']:            
-    if xy=='features':
-      end='fixed'
-    else:
-      end=''
-    a=np.load(root+'CNN'+xy+mode+name[0]+end+'.npy')
-    for n in name[1:]:
-      b=np.load(root+'CNN'+xy+mode+n+end+'.npy')
-      a=np.append(a,b,axis=0)
-    print(a.shape)
-    print(root+'CNN'+xy+mode+'AL'+end+'.npy')
-    np.save(root+'CNN'+xy+mode+'AL'+end+'.npy', a)
+    for xy in ['features','labels']:            
+        if xy=='features':
+            end='fixed'
+        else:
+            end=''
+        a=np.load(root+'CNN'+xy+mode+name[0]+end+'.npy')
+        for n in name[1:]:
+            b=np.load(root+'CNN'+xy+mode+n+end+'.npy')
+            a=np.append(a,b,axis=0)
+        print(a.shape)
+        print(root+'CNN'+xy+mode+'AL'+end+'.npy')
+        np.save(root+'CNN'+xy+mode+'AL'+end+'.npy', a)
