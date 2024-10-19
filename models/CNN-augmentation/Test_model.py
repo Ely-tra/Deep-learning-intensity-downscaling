29b56ba8727cf40957441ca03d64e2c34ba016f5
Chanh Kieu <ckieu@login1.hsn0.cm.bigred200.uits.iu.edu>
2024-10-19 17:50:41 -0400

    cleaning up further VIT code

--- models/CNN-augmentation/Test_model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import numpy as np
-import libtcg_utils as tcg_utils
-import os
-def resize_preprocess(image, HEIGHT, WIDTH, method):
-    image = tf.image.resize(image, (HEIGHT, WIDTH), method=method)
-    return image
-root=
--- models/CNN-augmentation/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python CNN_s1.py 2>&1 | tee logfile.log
