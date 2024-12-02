dc2c5c3fc348e7487ea23942a74d76b1014c332e
Ely-tra <157906084+Ely-tra@users.noreply.github.com>
2024-12-01 19:59:43 -0500

    Updated workflow, testing...

--- models/TC-net-cnn/job_br200.sh
+++ models/TC-net-cnn/job_br200.sh
@@ -1,40 +1,87 @@
 #!/bin/bash -l
 #SBATCH -N 1
 #SBATCH -t 48:00:00
-#SBATCH -J check_CNN
+#SBATCH -J check_VIT
 #SBATCH -p gpu --gpus 1
 #SBATCH -A r00043
 #SBATCH --mem=128G
 module load PrgEnv-gnu
 module load python/gpu/3.10.10
-cd /N/u/ckieu/BigRed200/model/Deep-learning-intensity-downscaling/models/TC-net-cnn/
+cd /N/slate/trihnguy/Deep-learning-intensity-downscaling/models/TC-net-ViT/
 #
 # Set up experiment parameters, which will be used to generate Python scripts.
 # These parameters are currently hardwired in each script.
 #
-windowsize='[19,19]'
-num_var=13
-x_size=64
-y_size=64
-kernel_size=7
-nfold=10
-seasonal='NO
-extra_info='NO'
-kfold='NO'
+windowsize_x=18
+windowsize_y=18
+var_num=13
+mode='VMAX' #VMAX PMIN RMW
+st_embed='--st_embed'  # Include if you want space-time embedding, otherwise leave empty
+learning_rate=0.001
+batch_size=256
+num_epochs=100
+image_size=64
+model_name='ViT_model2'
+validation_years=(2014)  # Specify validation years here
+test_years=(2017)        # Specify test years here
 datapath='/N/project/Typhoon-deep-learning/data/nasa-merra2/'
-workdir='/N/project/Typhoon-deep-learning/output/'
+workdir='/N/project/Typhoon-deep-learning/output-Tri/'
 besttrack='/N/project/hurricane-deep-learning/data/tc/ibtracs.ALL.list.v04r00.csv'
+inputpath='/N/project/Typhoon-deep-learning/output-Tri/TC_domain/'
+list_vars=("U850" "V850" "T850" "RH850" "U950" "V950" "T950" "RH950" "U750" "V750" "T750" "RH750" "SLP750")
+list_vars="${list_vars[@]}"
+# This is a proposed workflow
 #
-# runing the entire workflow
-#
-#python MERRA2tc_domain.py $datapath $workdir $besttrack $windowsize $num_var
-#python TC-extract_data.py $workdir $windowsize $num_var
-#python TC-CA_NaN_filling.py $workdir $windowsize $num_var
-#python TC-Split.py $workdir $windowsize $num_var
-#python TC-CNN_model.py VMAX $workdir $windowsize $num_var $kernel_size $x_size $y_size
-#python TC-CNN_model.py PMIN $workdir $windowsize $num_var $kernel_size $x_size $y_size 
-#python TC-CNN_model.py RMW  $workdir $windowsize $num_var $kernel_size $x_size $y_size
-#python TC-plot-testdata.py VMAX $workdir $windowsize $num_var $kernel_size $x_size $y_size
-#python TC-plot-testdata.py PMIN $workdir $windowsize $num_var $kernel_size $x_size $y_size
-#python TC-plot-testdata.py RMW  $workdir $windowsize $num_var $kernel_size $x_size $y_size
 
+python MERRA2tc_domain.py \
+    --csvdataset "$besttrack" \ #
+    --datapath "$datapath" \ #
+    --outputpath "$workdir" \ #
+    --windowsize "$windowsize_x" "$windowsize_y" \ #
+    --regions EP NA WP \ #
+    --minlat -90.0 --maxlat 90.0 \
+    --minlon -180.0 --maxlon 180.0 \
+    --maxwind 10000 --minwind 0 \
+    --maxpres 10000 --minpres 0 \
+    --maxrmw 10000 --minrmw 0
+
+python TC-extract_data_TSU.py \
+    --inputpath "$inputpath" \
+    --workdir "$workdir" \
+    --windowsize "$windowsize_x" "$windowsize_y" \
+    --list_vars $list_vars \
+    --force_rewrite
+
+python TC-CA_NaN_filling_kfold.py \
+    --workdir "$workdir" \
+    --windowsize "$windowsize_x" "$windowsize_y" \
+    --var_num "$var_num"
+
+#python TC-Split_KFold.py --workdir $workdir --windowsize $windowsize_x $windowsize_y --kfold $xfold --var_num $var_num
+
+python TC-build_model.py \
+    --mode $mode \
+    --root $workdir \
+    --windowsize $windowsize_x $windowsize_y \
+    --var_num $var_num \
+    --x_size $x_size \
+    --y_size $y_size \
+    --st_embed $st_embed\
+    --model_name $model_name \
+    --learning_rate $learning_rate \
+    --batch_size $batch_size \
+    --num_epochs $num_epochs \
+    --image_size $image_size \
+    --validation_year "${validation_years[@]}" \
+    --test_year "${test_years[@]}"
+
+python TC-test_plot.py \
+    --mode $mode \
+    --workdir $workdir \
+    --windowsize $windowsize_x $windowsize_y \
+    --var_num $var_num \
+    --image_size $image_size \
+    --st_embed $st_embed \
+    --model_name $model_name \
+    --validation_year "${validation_years[@]}" \
+    --test_year "${test_years[@]}"
