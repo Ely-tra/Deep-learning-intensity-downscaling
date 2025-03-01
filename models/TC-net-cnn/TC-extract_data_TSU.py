947b4f1b7a5743e868b2902f40f676cfcca52194
Ely-tra <157906084+Ely-tra@users.noreply.github.com>
2025-03-01 00:38:04 -0500

    Fixed some pathway related problems

--- models/TC-net-cnn/TC-extract_data_TSU.py
+++ models/TC-net-cnn/TC-extract_data_TSU.py
@@ -259,7 +259,7 @@ if __name__ == "__main__":
     print('Initiating.', flush=True)
     print('Initiation completed.', flush=True)
 
-    outputpath = os.path.join(workdir, f'exp_{var_num}features_{windowsize[0]}x{windowsize[1]}', 'data')
+    outputpath = os.path.join(workdir, 'Domain_data', f'exp_{var_num}features_{windowsize[0]}x{windowsize[1]}', 'data')
 
     if not os.path.exists(inputpath):
         print(f"Must have the input data from Step 1 by now....exit {inputpath}")
